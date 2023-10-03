77d791df5456394463e3ad62d07113955b5422a8
eduardokaique99 <eduardo_kaique99@hotmail.com>
2023-10-02 21:36:52 -0300

    02/10/23 - att2

---
+++ assets/js/dom.js
@@ -0,0 +1,61 @@
+console.log("DOM");
+
+const h1 = document.querySelector("h1");
+
+h1.innerText = "Título 1";
+h1.innerHTML += "<span>Título com span</span>";
+
+console.log(h1);
+
+document.body.append(h1);
+
+h1.innerText = "Mudei de novo";
+
+function criarElementos(data) {
+    const h1 = document.createElement("h1");
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    h1.innerText = data.titulo;
+    p.innerText = data.descricao;
+    div.append(h1);
+    div.append(p);
+    document.body.append(div);
+}
+
+const busca = fetch("https://dummyjson.com/users")
+    .then(
+        function (response) {
+            console.log("Dado que veio sem tratamento: ",response);    
+            return response.json();
+        }
+    )
+    .then(
+        function (data){
+            const usuarios = data.users;
+
+            usuarios.forEach(
+                (usuario) => 
+                    criarElementos({
+                        titulo: usuario.firstName,
+                        texto: usuario.email
+                    })
+            )
+
+        }
+    );
+
+const funcionario = [
+    {
+        nome: "Juana",
+        email: "juana@bol.com.br"
+    },
+    {
+        nome: "Carlos",
+        email: "carlos@bol.com.br"
+    },
+    {
+        nome: "Maria",
+        email: "madu@bol.com.br"
+    },
+];
+
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,12 +2,12 @@ console.log("Olá mundo")
 
 // função simples
 function figaOi() {
-    alert("Oi");
+    //alert("Oi");
 };
 
 // função anonima - não tem nome
 const digaOi2 = () => {
-    alert("Oi2");
+    //alert("Oi2");
 };
 
 // função anonima - de retorno
@@ -16,7 +16,7 @@ const digaOi3 = () => "Oi3";
 // função anonima - de retorno com parametro
 const digaOi4 = (nome) => "Oi4" + nome;
 
-alert(digaOi4("Juana"));
+//alert(digaOi4("Juana"));
 
  // função anonima - de retorno com parametro utilizando template string com crase
 const digaOi5 = (nome) => `Oi5 ${nome}`;
